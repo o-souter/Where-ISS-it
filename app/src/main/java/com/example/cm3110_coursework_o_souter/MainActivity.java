9f4989c80b004a652405a8ccfef1a1d4a5b8d76f
Oliver <o.souter@rgu.ac.uk>
2022-10-27 20:05:00 +0100

    basic first set-up commit

---
+++ app/src/main/java/com/example/cm3110_coursework_o_souter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cm3110_coursework_o_souter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
