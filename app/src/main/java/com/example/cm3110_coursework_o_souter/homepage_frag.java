be4970e8f9faf26a4ef6eefedcfc5dcfb396b6af
2004076 <o.souter@rgu.ac.uk>
2022-11-28 18:20:17 +0000

    fixed navigation graph issues

--- app/src/main/java/com/example/cm3110_coursework_o_souter/homepage_frag.java
+++ app/src/main/java/com/example/cm3110_coursework_o_souter/homepage_frag.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
+ * Use the {@link homepage_frag#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BlankFragment extends Fragment {
+public class homepage_frag extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class BlankFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public BlankFragment() {
+    public homepage_frag() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class BlankFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
+     * @return A new instance of fragment homepage_frag.
      */
     // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2) {
-        BlankFragment fragment = new BlankFragment();
+    public static homepage_frag newInstance(String param1, String param2) {
+        homepage_frag fragment = new homepage_frag();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class BlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.iss_locator_frag, container, false);
+        return inflater.inflate(R.layout.homepage_frag, container, false);
     }
 }
